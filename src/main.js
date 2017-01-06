db75cfec74cccd9f410e6555b521ee66ba5d2b7e
storiaca <storiaca@gmail.com>
2017-01-06 11:49:15 +0100

    Add vue-router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import vueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App'
+import Users from './components/Users'
+import Hello from './components/Hello'
 
-Vue.use(vueResource)
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        {path:'/', component: Users},
+        {path:'/hello', component: Hello}
+    ]
+});
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+  router,
+  template: `
+    <div id="app">
+        <ul>
+            <li><router-link to="/">Users</router-link></li>
+            <li><router-link to="/hello">Hello</router-link></li>
+        </ul>
+        <router-view></router-view>
+    </div>
+  `,
+}).$mount('#app')
